f1ec7f22d9409a6b0a35d4afab5e94c96bfe3508
kolianos007 <kolianos007@gmail.com>
2023-03-02 10:21:21 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const App = () => {
+  return <div className={'bg'}>Hello!</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const PrettierPlugin = require('prettier-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['tsx', 'ts', 'jsx', 'js'],
+      failOnError: true,
+    }),
+    new PrettierPlugin(),
+    new StylelintPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+};
